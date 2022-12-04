52de405633089d7a4c51f9c870304b9608cd9752
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-12-03 22:50:35 -0800

    sequential check

--- v1/check.go
+++ v1/check.go
@@ -1,5 +1,41 @@
 package gochainanalysis
 
-func (c *Client) CheckAll() {
-	
+import (
+	"go-chainanalysis/v1/oraclesanctions"
+)
+
+func (c *Client) IsSanctioned(walletAddress string) (bool, *oraclesanctions.OracleSanctionListNetwork, error) {
+	if _, err := c.OracleNetworks.Ethereum.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Ethereum, err
+	}
+	if _, err := c.OracleNetworks.Celo.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Celo, err
+	}
+	if _, err := c.OracleNetworks.Fantom.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Fantom, err
+	}
+	if _, err := c.OracleNetworks.Arbitrum.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Arbitrum, err
+	}
+	if _, err := c.OracleNetworks.Optimism.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Optimism, err
+	}
+	if _, err := c.OracleNetworks.Avalance.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Avalance, err
+	}
+	if _, err := c.OracleNetworks.Binance.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Binance, err
+	}
+	if _, err := c.OracleNetworks.Polygon.IsSanctioned(walletAddress); err == nil {
+	} else {
+		return true, c.OracleNetworks.Polygon, err
+	}
+	return false, nil, nil
 }
